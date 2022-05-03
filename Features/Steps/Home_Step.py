0219eb1f84ae5323dac4d00c6e4ba46fc6f667a9
HarishEkal <hekal@pointhealth.com>
2022-05-03 17:25:38 +0530

    login demo scenario

--- Features/Steps/Home_Step.py
+++ Features/Steps/Home_Step.py
@@ -1,23 +1,35 @@
 from behave import *
 
-#use_step_matcher("re")
+# use_step_matcher("re")
 use_step_matcher("parse")
+
 
 @given("User is on app home page")
 def step_impl(context):
     pass
 
+@step("User taps on Already have an account button")
+def step_impl(context):
+    context.Homepage.Already_acc()
+
 
 @step('User enters username "{username}"')
-def step_impl(context,username):
+def step_impl(context, username):
     context.Homepage.enter_Username(username)
 
 
 @step('User enters password "{password}"')
-def step_impl(context,password):
-    context.Homepage.enter_Password(password);
+def step_impl(context, password):
+    context.Homepage.enter_Password(password)
 
 
-@step('USer enters the username "{username}"')
-def step_impl(context,username):
-    context.Homepage.enter_Username(username)
+@step("User taps on Login button")
+def step_impl(context):
+    context.Homepage.clickOn_Login()
+
+@then("Homepage is displayed")
+def step_impl(context):
+    pass
+
+
+
