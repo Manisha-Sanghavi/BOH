7af04404e77228888f6f3afc65c47194a1b80a49
HarishEkal <hekal@pointhealth.com>
2022-05-13 18:16:16 +0530

    android boh apk code

--- Features/environment.py
+++ Features/environment.py
@@ -8,8 +8,9 @@ from allure_commons._allure import attach
 from Features.Pages.BasePage import Basepage
 #from Features.Utils.APIUtility import APIUtility
 from Features.Pages.Home_Page import Home_page
+from Features.Pages.bohapk_page import bohapk_page
 
-data = json.load(open("Features/Resources/config.json"))
+# data = json.load(open("Features/Resources/config.json"))
 
 # defbefore_all(context):
 
@@ -28,7 +29,7 @@ def before_scenario(context, scenario):
             command_executor='https://' + context.config.userdata["userName"] + ':' + context.config.userdata[
                 "accessKey"] + '@hub-cloud.browserstack.com/wd/hub',
             desired_capabilities={
-                "platformName": "iOS",
+                # "platformName": "Android",
                 "build": context.config.userdata["browserstack_build"],
                 "device": context.config.userdata["browserstack_device"],
                 "os_version": context.config.userdata["device_os_version"],
@@ -68,7 +69,7 @@ def before_scenario(context, scenario):
     context.driver.switch_to.context('NATIVE_APP')
     baseobject = Basepage(context.driver)
     context.Homepage = Home_page(baseobject)
-
+    context.boh = bohapk_page(baseobject)
     context.stepid = 1
 
 def after_step(context, step):
