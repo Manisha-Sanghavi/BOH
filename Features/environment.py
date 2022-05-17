1ee1f47749fed666b0705fdf852382e85458055a
HarishEkal <hekal@pointhealth.com>
2022-05-17 17:05:25 +0530

    modified scenario 2 & 6

--- Features/environment.py
+++ Features/environment.py
@@ -8,7 +8,7 @@ from allure_commons._allure import attach
 from Features.Pages.BasePage import Basepage
 #from Features.Utils.APIUtility import APIUtility
 from Features.Pages.Home_Page import Home_page
-from Features.Pages.SignIn_Page import bohapk_page
+from Features.Pages.SignIn_Page import SignIn_Page
 
 # data = json.load(open("Features/Resources/config.json"))
 
@@ -69,7 +69,7 @@ def before_scenario(context, scenario):
     context.driver.switch_to.context('NATIVE_APP')
     baseobject = Basepage(context.driver)
     context.Homepage = Home_page(baseobject)
-    context.boh = bohapk_page(baseobject)
+    context.boh = SignIn_Page(baseobject)
     context.stepid = 1
 
 def after_step(context, step):
