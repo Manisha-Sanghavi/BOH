4e59be18e98cd2a729d046a7c29f444b4fadbee3
Rashmi <rashmi.chitnis@spurqlabs.com>
2022-05-24 11:19:58 +0100

    RE 04 05 06 11 Done 07 in progress

--- Features/Pages/RE_Page.py
+++ Features/Pages/RE_Page.py
@@ -19,7 +19,7 @@ class RE_Page(Basepage):
 
     def re_tab(self):
         self.wait.until(
-            EC.presence_of_element_located((MobileBy.XPATH, "(//android.view.View)[9]"))).click()
+            EC.presence_of_element_located((MobileBy.XPATH, "(//android.widget.Button)[1]/following-sibling::android.view.View[3]"))).click()
 
     def get_re_num(self, re_string):
         a = re_string[4:7]
@@ -33,19 +33,6 @@ class RE_Page(Basepage):
         self.wait.until(
             EC.presence_of_element_located((MobileBy.XPATH, "(//android.view.View)[19]"))).click()
 
-
-    def verify_sortby_filterby(self, field):
-        a = self.driver.find_element(MobileBy.XPATH, "//android.widget.Button[@content-desc='"+field+"']")
-        s = a.get_attribute('content-desc')
-        print(s)
-        return  s
-
-    # def verify_filterby(self, field):
-    #     a = self.driver.find_element(MobileBy.XPATH, "//android.widget.Button[@content-desc='" + field + "']")
-    #     s = a.get_attribute('content-desc')
-    #     # print(s)
-    #     return s
-
     def tap_symbol(self):
         self.wait.until(
             EC.presence_of_element_located((MobileBy.XPATH, "(//android.widget.Button)[3]"))).click()
@@ -54,47 +41,77 @@ class RE_Page(Basepage):
         self.wait.until(
             EC.presence_of_element_located((MobileBy.XPATH, "//android.widget.ImageView[@content-desc='Add RE']"))).click()
 
-    def order_date(self):
+    def order_date(self, date):
         self.wait.until(
             EC.presence_of_element_located((MobileBy.XPATH, "//android.view.View[@content-desc='MM/DD/YYYY']"))).click()
         self.driver.find_element(MobileBy.XPATH, '//android.view.View[@content-desc="31, Tuesday, May 31, 2022"]').click()
         self.driver.find_element(MobileBy.XPATH, '//android.widget.Button[@content-desc="OK"]').click()
 
-    def cust_info(self):
+    def cust_info(self, field, value):
+        if field == "RE Region":
+            self.driver.find_element(MobileBy.XPATH,'(//android.view.View[@content-desc="Customer Info"]/following-sibling::android.view.View)[2]').click()
+            self.wait.until(
+                EC.presence_of_element_located((MobileBy.XPATH, '//android.widget.Button[@content-desc="'+value+'"]'))).click()
+        if field == "Customer Name":
+            self.driver.find_element(MobileBy.XPATH, '(//android.view.View[@content-desc="Customer Info"]/following-sibling::android.view.View)[3]').click()
+            self.wait.until(
+                EC.presence_of_element_located((MobileBy.XPATH, '//android.view.View[@content-desc="'+value+'"]'))).click()
+        if field == "Customer Location":
+            element_to_tap = self.driver.find_element(MobileBy.XPATH, '(//android.view.View[@content-desc="Customer Info"]/following-sibling::android.view.View)[3]')
+            element_to_drag_to = self.driver.find_element(MobileBy.XPATH, '(//android.view.View[@content-desc="Opportunity"])[1]')
+            self.driver.scroll(
+                element_to_tap, element_to_drag_to)
+            self.wait.until(
+                EC.presence_of_element_located((MobileBy.XPATH, '(//android.view.View[@content-desc="Customer Info"]/following-sibling::android.view.View)[4]'))).click()
+            self.wait.until(
+                EC.presence_of_element_located((MobileBy.XPATH, '// android.widget.EditText'))).send_keys(value)
+            self.wait.until(
+                EC.presence_of_element_located((MobileBy.XPATH, '(//android.view.View[@content-desc="'+value+'"])'))).click()
+
+    def scroll_down(self):
+        element_to_tap = self.driver.find_element(MobileBy.XPATH,
+                                                  '//android.widget.EditText[@text="Rank or Title (optional)"]')
+        element_to_drag_to = self.driver.find_element(MobileBy.XPATH,
+                                                      '(//android.view.View[@content-desc="Opportunity"])[1]')
+        self.driver.scroll(
+            element_to_tap, element_to_drag_to)
+
+    def tap_new(self):
         self.wait.until(
-            EC.presence_of_element_located((MobileBy.XPATH, "//android.widget.Button[@content-desc='Direct']"))).click()
-        self.driver.find_element(MobileBy.XPATH,
-                                 '(//android.view.View)[18]').click()
-        self.driver.find_element(MobileBy.XPATH, '//android.widget.Button[@content-desc="East"]').click()
-        self.driver.find_element(MobileBy.XPATH, '(//android.view.View)[19]').click()
-        self.wait.until(
-            EC.presence_of_element_located((MobileBy.XPATH, "//android.view.View[@content-desc='Joe M']"))).click()
-        self.driver.find_element(MobileBy.XPATH, '(//android.view.View)[20]')
-        self.driver.find_element(MobileBy.XPATH, '(//android.view.View[@content-desc="Amsterdam"])[1]')
+            EC.presence_of_element_located((MobileBy.XPATH, '//android.widget.Button[@content-desc="New"]'))).click()
 
     def primary_contact(self, field,value):
-        self.wait.until(
-            EC.presence_of_element_located((MobileBy.XPATH, "(// android.widget.Button)[5]"))).click()
-        if field == "Rank or Title (optional)":
-            rank = self.driver.find_element(MobileBy.XPATH, '// android.widget.EditText[ @ text = "'+field+'"]').click()
-            rank.send_keys(value)
-        if field == "Contact First Name":
-            first_name = self.driver.find_element(MobileBy.XPATH, '// android.widget.EditText[ @ text = "'+field+'"]').click()
-            first_name.send_keys(value)
-        if field == "Contact Last Name":
-            last_name = self.driver.find_element(MobileBy.XPATH, '// android.widget.EditText[ @ text = "'+field+'"]').click()
-            last_name.send_keys(value)
-        if field == "Contact Email":
-            email = self.driver.find_element(MobileBy.XPATH, '// android.widget.EditText[ @ text = "'+field+'"]').click()
-            email.send_keys(value)
-        if field == "Contact Phone":
-            phone = self.driver.find_element(MobileBy.XPATH, '// android.widget.EditText[ @ text = "'+field+'"]').click()
-            phone.send_keys(value)
+        a = self.driver.find_element(MobileBy.XPATH, '//android.widget.EditText[@text="'+field+'"]').click()
+        a.send_keys(value)
+        # if field == "Rank or Title (optional)":
+        #     rank = self.driver.find_element(MobileBy.XPATH, '// android.widget.EditText[ @ text = "'+field+'"]').click()
+        #     rank.send_keys(value)
+        # if field == "Contact First Name":
+        #     first_name = self.driver.find_element(MobileBy.XPATH, '// android.widget.EditText[ @ text = "'+field+'"]').click()
+        #     first_name.send_keys(value)
+        # if field == "Contact Last Name":
+        #     last_name = self.driver.find_element(MobileBy.XPATH, '// android.widget.EditText[ @ text = "'+field+'"]').click()
+        #     last_name.send_keys(value)
+        # if field == "Contact Email":
+        #     email = self.driver.find_element(MobileBy.XPATH, '// android.widget.EditText[ @ text = "'+field+'"]').click()
+        #     email.send_keys(value)
+        # if field == "Contact Phone":
+        #     phone = self.driver.find_element(MobileBy.XPATH, '// android.widget.EditText[ @ text = "'+field+'"]').click()
+        #     phone.send_keys(value)
 
     def select_metrics(self):
+        el1 = self.wait.until(
+            EC.presence_of_element_located((MobileBy.XPATH, '//android.view.View[@content-desc="Metrics"]'))).click()
+        el2 = self.wait.until(
+            EC.presence_of_element_located((MobileBy.XPATH, '(//android.widget.Button)[6]'))).click()
+        action = TouchAction(self.driver)
+        action.press(el1).move_to(el2).release().perform()
         sq_ft = self.wait.until(
             EC.presence_of_element_located((MobileBy.XPATH, "//android.widget.EditText[@text='Pre-RE Footprint (sq ft)']"))).click()
         sq_ft.send_keys("70")
+        user_action = TouchAction(self.driver)
+        user_action.tap(x=743, y=1328).perform()
+        user_action.tap(x=743, y=1562).perform()
         self.wait.until(
             EC.presence_of_element_located((MobileBy.XPATH, "//android.widget.Button[@content-desc='TAN']"))).click()
 
@@ -107,26 +124,22 @@ class RE_Page(Basepage):
             EC.presence_of_element_located(
                 (MobileBy.XPATH, "//android.widget.Button[@content-desc='Search']"))).click()
 
-    def search_re(self):
+    def search_re(self, number):
         re_num = self.wait.until(
             EC.presence_of_element_located(
                 (MobileBy.XPATH, "//android.widget.EditText[@text='Search REs']")))
-        re_num.send_keys("772")
+        re_num.send_keys("number")
 
-    def select_re(self):
-        # result = self.wait.until(
-        #     EC.presence_of_element_located(
-        #         (MobileBy.XPATH, "//android.widget.EditText[@text='Search REs']"))).click()
-        # result.send_keys("772")
+    def select_re(self, re_num):
         self.wait.until(
             EC.presence_of_element_located(
-                (MobileBy.XPATH, "//android.view.View[@content-desc='Camp Humphreys, Korea - RE 772']"))).click()
+                (MobileBy.XPATH, f"//android.view.View[@content-desc='Camp Humphreys, Korea - RE {re_num}']"))).click()
 
 
     def verify_re(self):
         ele = self.wait.until(
             EC.presence_of_element_located(
-                (MobileBy.XPATH, "(//android.view.View)[15]")))
+                (MobileBy.XPATH, "//android.widget.ScrollView/android.view.View[1]")))
         re_string = ele.get_attribute('content-desc')
         a = self.get_re_num(re_string)
         return a
@@ -145,10 +158,47 @@ class RE_Page(Basepage):
         return a
 
     def verify_fields(self, field):
-        a = self.driver.find_element(MobileBy.XPATH, "//android.widget.Button[@content-desc='" + field + "']")
+        a = self.wait.until(
+            EC.presence_of_element_located((MobileBy.XPATH, "//android.widget.ImageView[@content-desc='"+field+"']")))
         s = a.get_attribute('content-desc')
         print(s)
         return s
+
+    def select_re_from_list(self):
+        self.wait.until(
+            EC.presence_of_element_located((MobileBy.XPATH, "(// android.view.View)[21]"))).click()
+
+    def edit_subtitle(self, text):
+        user_action = TouchAction(self.driver)
+        user_action.tap(x=100,y=385).perform()
+        re_text = self.wait.until(
+            EC.presence_of_element_located((MobileBy.CLASS_NAME, "android.widget.EditText")))
+        re_text.send_keys("Requirements changed")
+        self.wait.until(
+            EC.presence_of_element_located((MobileBy.XPATH, "(//android.widget.Button)[3]"))).click()
+
+    def verify_subtitle(self):
+        ele = self.wait.until(
+            EC.presence_of_element_located((MobileBy.XPATH, "(//android.view.View)[15]")))
+        ele_string = ele.get_attribute('content-desc')
+        a = ele_string.split()
+        return a[2], a[3]
+
+    def select_re_status(self, re_status):
+        self.wait.until(
+            EC.presence_of_element_located((MobileBy.XPATH, "//android.widget.Button[@content-desc='"+re_status+"']"))).click()
+
+    def verify_re_status(self, re_status):
+        self.wait.until(
+            EC.presence_of_element_located(
+                (MobileBy.XPATH, "(// android.view.View)[21]"))).click()
+        uc_re = re_status.upper()
+        a = self.wait.until(EC.presence_of_element_located((MobileBy.XPATH, "//android.view.View[@content-desc='"+uc_re+"']")))
+        b = a.get_attribute('content-desc')
+        return b
+
+
+
 
 
 
--- Features/Steps/RE_Steps.py
+++ Features/Steps/RE_Steps.py
@@ -3,109 +3,78 @@ from behave import *
 #use_step_matcher("re")
 use_step_matcher("parse")
 
-@given("User is on BOH FPU homepage")
-def step_impl(context):
-    context.boh.dev_build()
-    print("User is on Homepage")
-
-
-@when('User login with "{username}" and "{password}"')
-def step_impl(context, username, password):
-    context.boh.enter_Username(username)
-    context.boh.enter_Password(password)
-    context.boh.clickOn_Login()
-    try:
-        context.boh.skip()
-    except:
-        print("skip is skipped")
-
-@step('user is navigated to "{message}" page')
-def step_impl(context, message):
-    context.boh.verify_page(message)
-
 @step("user taps on RE tab")
 def step_impl(context):
     context.re.re_tab()
-
 
 @step('user taps on "{option}" options')
 def step_impl(context, option):
     if option == "Sort By":
         context.re.click_sortby()
-    elif option == "Filter By":
+    if option == "Filter By":
         context.re.click_filterby()
 
 @then("user verifies following options are available for sort by")
 def step_impl(context):
-   #context.re.verify_dropdownlist()
-
-    for row in context.table:
-        s = context.re.verify_sortby_filterby(row['Field'])
+      for row in context.table:
+        s = context.re.verify_fields(row['Field'])
         assert row['Field'] == s
 
 @step("user taps on plus symbol")
 def step_impl(context):
     context.re.tap_symbol()
 
-
 @step("user taps on Add RE")
 def step_impl(context):
     context.re.add_re()
 
-
-@step('user selects Estimated Order Date "21/05/2022"')
-def step_impl(context):
-    context.re.order_date()
-
+@step('user selects Estimated Order Date "{date}"')
+def step_impl(context, date):
+    context.re.order_date(date)
 
 @step("user enter following details for selected Customer Info options")
 def step_impl(context):
-    context.re.cust_info()
-
-
-@step("user enters the Primary Contact details as:")
-def step_impl(context):
     for row in context.table:
-        context.re.primary_contact(row['Feild'], row['Value'])
+        context.re.cust_info(row['Field'], row['Value'])
 
+@step("user enters the Primary Contact details as")
+def step_impl(context):
+    context.re.tap_new()
+    context.re.scroll_down()
+    for row in context.table:
+        context.re.primary_contact(row['Field'], row['Value'])
 
 @step("user enter following details for Metrics as:")
 def step_impl(context):
     context.re.select_metrics()
 
-
 @step("user taps Create Requirements Estimate")
 def step_impl(context):
     context.re.create_estimation()
-
 
 @then("user verifies RE details page is displayed")
 def step_impl(context):
     pass
 
-
 @step('user taps on "search" symbol')
 def step_impl(context):
     context.re.tap_search()
 
+@step('user enters "{number}" in search field')
+def step_impl(context, number):
+    context.re.search_re(number)
 
-@step('user enters "772" in search field')
-def step_impl(context):
-    context.re.search_re()
-
-
-@step('user selects "RE #772"')
-def step_impl(context):
-    context.re.select_re()
+@step('user selects RE# "{re_num}"')
+def step_impl(context, re_num):
+    context.re.select_re(re_num)
 
 @then("user verifies following options are available for filter by")
 def step_impl(context):
     for row in context.table:
-        s = context.re.verify_sortby_filterby(row['Field'])
+        s = context.re.verify_fields(row['Field'])
         assert row['Field'] == s
 
-
-@then('user verifies "RE #772" details page is displayed')
+@then('user verifies searched RE details page is displayed')
 def step_impl(context):
     a = context.re.verify_re()
     assert a == "772"
@@ -113,7 +82,6 @@ def step_impl(context):
 @step('user taps on "downward arrow"')
 def step_impl(context):
     context.re.tap_arrow()
-
 
 @then('user verifies REs displayed are in ascending order of REID')
 def step_impl(context):
@@ -123,7 +91,6 @@ def step_impl(context):
     print(b)
     assert int(a) < int(b)
 
-
 @then("user verifies REs displayed are in descending order of REID")
 def step_impl(context):
     a = context.re.verify_ascend_descend(21)
@@ -132,9 +99,34 @@ def step_impl(context):
     print(b)
     assert int(a) > int(b)
 
-
 @then("user verifies following options are displayed")
 def step_impl(context):
     for row in context.table:
         s = context.re.verify_fields(row['Field'])
         assert row['Field'] == s
+
+@step('user selects RE# "{re_num}" from RE list')
+def step_impl(context, re_num):
+    context.re.select_re_from_list(re_num)
+
+@step('user edits subtitle as "{text}"')
+def step_impl(context, text):
+    context.re.edit_subtitle(text)
+
+@then('user verifies "Requirements changed" subtitle is displayed')
+def step_impl(context):
+    a, b = context.re.verify_subtitle()
+    assert a == "Requirements"
+    assert b == "changed"
+
+
+@step('user select "{RE_Status}" from dropdown list')
+def step_impl(context, RE_Status):
+    context.re.select_re_status(RE_Status)
+
+
+@then('user verifies REs with state "{RE_Status}" only are displayed')
+def step_impl(context, RE_Status):
+    a = context.re.verify_re_status(RE_Status)
+    b = RE_Status.upper()
+    assert a == b
